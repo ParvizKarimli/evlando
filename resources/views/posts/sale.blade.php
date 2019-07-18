e5a5cf330ff988efde1bb2c020e6578013dbcb81
Parviz Karimli <devparviz@gmail.com>
2019-07-18 09:56:28 +0400

    Better layout for posts index, sale, and rent views

--- resources/views/posts/sale.blade.php
+++ resources/views/posts/sale.blade.php
@@ -1,55 +1,61 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Posts</h1>
-<div class="form-group">
-    <label for="type">Type:</label>
-    <select name="type" site="posts" onchange="typeChange(this)">
-        <option value="all">All</option>
-        <option value="sale" selected>For Sale</option>
-        <option value="rent">For Rent</option>
-    </select>
+<h1 class="text-center">Posts</h1>
+<div class="col-md-2 col-sm-2">
+    <div class="well">
+        <div class="form-group">
+            <label for="type">Type:</label>
+            <select name="type" site="posts" onchange="typeChange(this)">
+                <option value="all">All</option>
+                <option value="sale" selected>For Sale</option>
+                <option value="rent">For Rent</option>
+            </select>
+        </div>
+    </div>
 </div>
-@if(count($posts) > 0)
-    @foreach($posts as $post)
-        <div class="well">
-            <div class="row">
-                <div class="col-md-4 col-sm-4">
-                    <img style="width:100%" src="/storage/images/cover_images/thumbnails/{{$post->thumbnail}}">
-                </div>
-                <div class="col-md-8 col-sm-8">
-                    <h3>
-                        <a href="/posts/{{$post->id}}">
-                            {{$post->title}}
-                        </a>
-                        @if(auth()->user())
-                            @if($bookmarked[$post->id] === false)
-                                <a href="" title="Bookmark this post" bookmark-post-id="{{$post->id}}" onclick="bookmarkPost(this);">
+<div class="col-md-10 col-sm-10">
+    @if(count($posts) > 0)
+        @foreach($posts as $post)
+            <div class="well">
+                <div class="row">
+                    <div class="col-md-4 col-sm-4">
+                        <img style="width:100%" src="/storage/images/cover_images/thumbnails/{{$post->thumbnail}}">
+                    </div>
+                    <div class="col-md-8 col-sm-8">
+                        <h3>
+                            <a href="/posts/{{$post->id}}">
+                                {{$post->title}}
+                            </a>
+                            @if(auth()->user())
+                                @if($bookmarked[$post->id] === false)
+                                    <a href="" title="Bookmark this post" bookmark-post-id="{{$post->id}}" onclick="bookmarkPost(this);">
+                                        <i class="far fa-star"></i>
+                                    </a>
+                                @elseif($bookmarked[$post->id] === true)
+                                    <a href="" title="Remove this post from bookmarks" bookmark-post-id="{{$post->id}}" onclick="bookmarkPost(this);">
+                                        <i class="fas fa-star"></i>
+                                    </a>
+                                @endif
+                            @elseif(auth()->guest())
+                                <a href="{{route('login')}}" title="Bookmark this post">
                                     <i class="far fa-star"></i>
                                 </a>
-                            @elseif($bookmarked[$post->id] === true)
-                                <a href="" title="Remove this post from bookmarks" bookmark-post-id="{{$post->id}}" onclick="bookmarkPost(this);">
-                                    <i class="fas fa-star"></i>
-                                </a>
                             @endif
-                        @elseif(auth()->guest())
-                            <a href="{{route('login')}}" title="Bookmark this post">
-                                <i class="far fa-star"></i>
-                            </a>
-                        @endif
-                    </h3>
-                    <small>
-                        Created at {{$post->created_at}} by {{$post->user->name}}
-                    </small>
-                    <div>
-                        {!! str_limit($post->body, $limit = 150, $end = '...') !!}
+                        </h3>
+                        <small>
+                            Created at {{$post->created_at}} by {{$post->user->name}}
+                        </small>
+                        <div>
+                            {!! str_limit($post->body, $limit = 150, $end = '...') !!}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    @endforeach
-    {{$posts->links()}}
-@else
-    <p>No posts found.</p>
-@endif
+        @endforeach
+        {{$posts->links()}}
+    @else
+        <p>No posts found.</p>
+    @endif
+</div>
 @endsection
