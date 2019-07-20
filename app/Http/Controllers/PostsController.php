329e30411528a2052707c5127b046cece6d2e3f7
Parviz Karimli <devparviz@gmail.com>
2019-07-20 17:11:11 +0400

    Made some (design) changes to location suggestion

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -540,7 +540,7 @@ class PostsController extends Controller
 
             foreach($locations as $location)
             {
-                echo '<p><a href="" onclick="selectLocation(this);" location-id="'. $location->id .'">' . $location->city . ', ' . $location->province . ', ' . $location->country . '</a></p>';
+                echo '<p><a class="location-suggestion" title="Click to select the location" href="" onclick="selectLocation(this);" location-id="'. $location->id .'">' . $location->city . ', ' . $location->province . ', ' . $location->country . '</a></p>';
             }
         }
     }
--- public/js/select_location.js
+++ public/js/select_location.js
@@ -3,4 +3,5 @@ function selectLocation(e) {
 
     var locationSelected = e.innerText;
     document.getElementById('location').value = locationSelected;
+    document.getElementById('location-suggestions').style.display = 'none';
 }
