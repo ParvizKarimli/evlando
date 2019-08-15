ecf07d8a48421597c8360e1604610853a68f2539
Parviz Karimli <devparviz@gmail.com>
2019-08-15 12:24:39 +0400

    Added modal to pages/index (posts) & posts/show views

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -63,9 +63,31 @@
                         </a>
                     @endif
 
-                    <a class="report-post" href="" title="Report this post">
+                    <!-- Trigger the modal with a button -->
+                    <a href="" title="Report this post" data-toggle="modal" data-target="#reportModal">
                         <i class="fas fa-flag"></i>
                     </a>
+
+                    <!-- Modal -->
+                    <div id="reportModal" class="modal fade" role="dialog">
+                        <div class="modal-dialog">
+
+                            <!-- Modal content-->
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    <h4 class="modal-title">Modal Header</h4>
+                                </div>
+                                <div class="modal-body">
+                                    <p>Some text in the modal.</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
                 @elseif(auth()->guest())
                     <a href="{{route('login')}}" title="Bookmark this post">
                         <i class="far fa-star"></i>
