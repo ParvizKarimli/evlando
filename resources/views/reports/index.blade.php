15c919926b0289cb2d86b28ec26b54b1a5fde6de
Parviz Karimli <devparviz@gmail.com>
2019-08-16 10:53:20 +0400

    Added columns to reports/index view

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -21,16 +21,50 @@
 
                 <h3>Reports</h3>
                 @if(count($reports) > 0)
-                    <table class="table table-striped">
+                    <table class="table">
                         <tr>
+                            <th></th>
                             <th>ID</th>
+                            <th>Reporter User ID</th>
+                            <th>Reported User/Post ID</th>
+                            <th>Category</th>
+                            <th>Message</th>
                             <th>Created at</th>
                             <th>Updated at</th>
                             <th></th>
                         </tr>
                         @foreach($reports as $report)
-                            <tr>
-                                <td>{{$report->id}}</td>
+                            <tr @if($report->seen === 0) class="alert alert-info" @endif>
+                                <td>
+                                    @if($report->resolved === 1)
+                                        <h4 class="alert-success" title="Resolved">&#10004;</h4>
+                                    @endif
+                                </td>
+                                <td>
+                                    <a href="/reports/{{$report->id}}">
+                                        {{$report->id}}
+                                    </a>
+                                </td>
+                                <td>{{$report->reporter_user_id}}</td>
+                                @if($report->reported_user_id)
+                                    <td>{{$report->reported_user_id}}</td>
+                                @else
+                                    <td>{{$report->post_id}}</td>
+                                @endif
+                                @if($report->category === 1)
+                                    <td>Spam</td>
+                                @elseif($report->category === 2)
+                                    <td>Nudity</td>
+                                @elseif($report->category === 3)
+                                    <td>Hate speech</td>
+                                @elseif($report->category === 4)
+                                    <td>Other</td>
+                                @endif
+                                <td>
+                                    <a href="/reports/{{$report->id}}">
+                                        {{str_limit($report->message, $limit = 15, $end = '...')}}
+                                    </a>
+                                </td>
                                 <td>{{$report->created_at}}</td>
                                 <td>{{$report->updated_at}}</td>
                                 <td>
