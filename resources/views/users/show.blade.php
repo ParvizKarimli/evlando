03cc71f854df56c7887f16d83340547bfe1b7198
Parviz Karimli <devparviz@gmail.com>
2019-08-16 15:30:57 +0400

    Kind of better document structure for pages/index, posts/show, and users/show views

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -123,73 +123,72 @@
                                             <a class="pull-right" href="" title="Report this post" data-toggle="modal" data-target="#reportModal-{{$post->id}}">
                                                 &#9873
                                             </a>
-                                        @endif
                                     </h1>
+                                            <!-- Modal -->
+                                            <div id="reportModal-{{$post->id}}" class="modal fade" role="dialog">
+                                                <div class="modal-dialog">
 
-                                    <!-- Modal -->
-                                    <div id="reportModal-{{$post->id}}" class="modal fade" role="dialog">
-                                        <div class="modal-dialog">
+                                                    <!-- Modal content-->
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                                            <h4 class="modal-title">Report Post</h4>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            {!! Form::open(['action' => 'ReportsController@store', 'method' => 'POST']) !!}
+                                                                <div class="form-group {{ $errors->has('category') ? 'has-error' : '' }}">
+                                                                    <label for="category">Category (required)</label>
+                                                                    <div class="radio">
+                                                                        <label>
+                                                                            <input type="radio" name="category" value="1" required> Spam
+                                                                        </label>
+                                                                    </div>
+                                                                    <div class="radio">
+                                                                        <label>
+                                                                            <input type="radio" name="category" value="2" required> Nudity
+                                                                        </label>
+                                                                    </div>
+                                                                    <div class="radio">
+                                                                        <label>
+                                                                            <input type="radio" name="category" value="3" required> Hate speech
+                                                                        </label>
+                                                                    </div>
+                                                                    <div class="radio">
+                                                                        <label>
+                                                                            <input type="radio" name="category" value="4" required> Other
+                                                                        </label>
+                                                                    </div>
+                                                                    @if($errors->has('category'))
+                                                                        <span class="help-block">
+                                                                            <strong>{{ $errors->first('category') }}</strong>
+                                                                        </span>
+                                                                    @endif
+                                                                </div>
+                                                                <div class="form-group {{ $errors->has('message') ? 'has-error' : '' }}">
+                                                                    {{Form::label('message', 'Message (optional)')}}
+                                                                    {{Form::textarea('message', '', ['rows' => 4, 'class' => 'form-control', 'placeholder' => 'Message'])}}
+                                                                    @if($errors->has('message'))
+                                                                        <span class="help-block">
+                                                                            <strong>{{ $errors->first('message') }}</strong>
+                                                                        </span>
+                                                                    @endif
+                                                                </div>
+                                                                {!! Form::hidden('reported_type', 'post') !!}
+                                                                {!! Form::hidden('post_id', $post->id) !!}
+                                                                {!! Form::hidden('post_owner_id', $post->user_id) !!}
+                                                                <div class="form-group">
+                                                                    {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
+                                                                </div>
+                                                            {!! Form::close() !!}
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                                        </div>
+                                                    </div>
 
-                                            <!-- Modal content-->
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                                    <h4 class="modal-title">Report Post</h4>
-                                                </div>
-                                                <div class="modal-body">
-                                                    {!! Form::open(['action' => 'ReportsController@store', 'method' => 'POST']) !!}
-                                                        <div class="form-group {{ $errors->has('category') ? 'has-error' : '' }}">
-                                                            <label for="category">Category (required)</label>
-                                                            <div class="radio">
-                                                                <label>
-                                                                    <input type="radio" name="category" value="1" required> Spam
-                                                                </label>
-                                                            </div>
-                                                            <div class="radio">
-                                                                <label>
-                                                                    <input type="radio" name="category" value="2" required> Nudity
-                                                                </label>
-                                                            </div>
-                                                            <div class="radio">
-                                                                <label>
-                                                                    <input type="radio" name="category" value="3" required> Hate speech
-                                                                </label>
-                                                            </div>
-                                                            <div class="radio">
-                                                                <label>
-                                                                    <input type="radio" name="category" value="4" required> Other
-                                                                </label>
-                                                            </div>
-                                                            @if($errors->has('category'))
-                                                                <span class="help-block">
-                                                                    <strong>{{ $errors->first('category') }}</strong>
-                                                                </span>
-                                                            @endif
-                                                        </div>
-                                                        <div class="form-group {{ $errors->has('message') ? 'has-error' : '' }}">
-                                                            {{Form::label('message', 'Message (optional)')}}
-                                                            {{Form::textarea('message', '', ['rows' => 4, 'class' => 'form-control', 'placeholder' => 'Message'])}}
-                                                            @if($errors->has('message'))
-                                                                <span class="help-block">
-                                                                    <strong>{{ $errors->first('message') }}</strong>
-                                                                </span>
-                                                            @endif
-                                                        </div>
-                                                        {!! Form::hidden('reported_type', 'post') !!}
-                                                        {!! Form::hidden('post_id', $post->id) !!}
-                                                        {!! Form::hidden('post_owner_id', $post->user_id) !!}
-                                                        <div class="form-group">
-                                                            {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
-                                                        </div>
-                                                    {!! Form::close() !!}
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                                                 </div>
                                             </div>
-
-                                        </div>
-                                    </div>
+                                        @endif
                                 </div>
                                 @if($post->type === 'sale')
                                     <p class="alert-info">{{ucfirst($post->property_type)}} For Sale</p>
