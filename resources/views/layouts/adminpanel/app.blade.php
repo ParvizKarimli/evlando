0d33f89e51e69910fdbe02ac1ed0cd56cd2070af
Parviz Karimli <devparviz@gmail.com>
2019-09-13 13:16:12 +0400

    Added website icon to adminpanel/app

--- resources/views/layouts/adminpanel/app.blade.php
+++ resources/views/layouts/adminpanel/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>{{ config('app.name', 'Evlando') }} - @yield('title')</title>
+    <link rel="shortcut icon" href="/storage/images/default/59763.png">
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
