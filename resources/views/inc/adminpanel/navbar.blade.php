d53f5cc6aaa3d95debe0c4e0b6e86c96a08078db
Parviz Karimli <devparviz@gmail.com>
2019-09-17 10:49:04 +0400

    Fixed undefined number_of_unseen_reports variable error when a user who is not admin or mod visits their dashboard

--- resources/views/inc/adminpanel/navbar.blade.php
+++ resources/views/inc/adminpanel/navbar.blade.php
@@ -129,7 +129,7 @@
     </div>
 </nav>
 
-@if($number_of_unseen_reports > 10)
+@if(!empty($number_of_unseen_reports) && $number_of_unseen_reports > 10)
     @section('infinite_scroll')
     <script>
         $('.reports-container').infiniteScroll({
